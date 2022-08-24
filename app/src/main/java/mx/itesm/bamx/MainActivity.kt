778f70a45eb3f7265627ff173381bce75cb298f5
Lydiadel <lydia101-lidi@hotmail.com>
2022-08-24 13:21:57 -0500

    Add Android Studio files

---
+++ app/src/main/java/mx/itesm/bamx/MainActivity.kt
@@ -0,0 +1,20 @@
+package mx.itesm.bamx
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /*
+    fun Maps(view: View?){
+        val intent = Intent(this, MapsActivity::class.java)
+
+        startActivity(intent)
+    }*/
+}
