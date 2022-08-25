8e281b3fd3bf995c897f5d2dc840bcfc21bf5b42
Jonathan J Quirino <75098397+jquirinoc-dev@users.noreply.github.com>
2022-08-25 17:11:40 -0500

    Image resources added. Sign in page design done (Could change)

--- app/src/main/java/mx/itesm/bamx/MainActivity.kt
+++ app/src/main/java/mx/itesm/bamx/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,4 +18,12 @@ class MainActivity : AppCompatActivity() {
 
         startActivity(intent)
     }*/
+
+    fun next(view: View?){
+
+    }
+
+    fun test(view: View?){
+        Toast.makeText(this, "Imagen clickeada", Toast.LENGTH_SHORT).show()
+    }
 }
