7a9391f6de1d5d5fa22effa046cb873fe5372a29
Jonathan J Quirino <75098397+jquirinoc-dev@users.noreply.github.com>
2022-08-26 07:54:19 -0500

    Donation activity

--- app/src/main/java/mx/itesm/bamx/DonationActivity.kt
+++ app/src/main/java/mx/itesm/bamx/DonationActivity.kt
@@ -1,11 +1,19 @@
 package mx.itesm.bamx
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class DonationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_donation)
+    }
+
+    fun goToMapHome(view : View?){
+
+        val intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
     }
 }
