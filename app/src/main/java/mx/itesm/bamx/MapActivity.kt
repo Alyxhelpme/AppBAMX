361690a1cd409b8b3963541b08c5ddcbbe171b50
Jonathan J Quirino <75098397+jquirinoc-dev@users.noreply.github.com>
2022-08-31 18:43:26 -0500

    Google Login seems to be working fine for now

--- app/src/main/java/mx/itesm/bamx/MapActivity.kt
+++ app/src/main/java/mx/itesm/bamx/MapActivity.kt
@@ -4,11 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class MapActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var welcome : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        welcome = findViewById(R.id.top)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        val email = firebaseUser!!.email
+        welcome.text = "Logged in as $email"
+
     }
 
     fun goToDonations(view: View?){
