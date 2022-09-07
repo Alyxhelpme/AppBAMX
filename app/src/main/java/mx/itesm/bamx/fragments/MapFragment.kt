e45e8f5df22f036aefc77074dcc6ff557c4e7762
Gael <gaelrodguez@hotmail.com>
2022-09-06 20:53:23 -0500

    Ya se obtienen las ubicaciones desde Firebase y se muestran el marcador en la ubicacion

--- app/src/main/java/mx/itesm/bamx/fragments/MapFragment.kt
+++ app/src/main/java/mx/itesm/bamx/fragments/MapFragment.kt
@@ -1,22 +1,33 @@
 package mx.itesm.bamx.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.DocumentChange
 import mx.itesm.bamx.R
 
 
 class MapFragment : Fragment(), OnMapReadyCallback {
 
-    private lateinit var map:GoogleMap
+    private lateinit var map : GoogleMap
+    private lateinit var collection : CollectionReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+         collection = Firebase.firestore.collection("centers")
+        fetchCenters()
     }
 
     override fun onCreateView(
@@ -39,5 +50,26 @@ class MapFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(p0: GoogleMap) {
         map = p0
+        //Toast.makeText(this.context,"HOLA", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun fetchCenters(){
+        val requestCenters = collection.get()
+
+        requestCenters.addOnSuccessListener {
+            result ->
+            for(document in result){
+                val lat = document.getDouble("lat")
+                val lng = document.getDouble("lng")
+                var coordinates : LatLng
+                if(lat != null && lng != null){
+                    coordinates = LatLng(lat, lng)
+                    map.addMarker(MarkerOptions().position(coordinates).title("Center"))
+                }
+            }
+        }. addOnFailureListener{
+            error ->
+            Log.e("Firestore", "error: $error")
+        }
     }
 }
