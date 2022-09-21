4da24c7fd1d6d2b2b36dc239f70bceaacecf633a
Juan Pablo <jppd0657@gmail.com>
2022-09-21 09:53:03 -0500

    comment

--- app/src/main/java/mx/itesm/bamx/fragments/HomeFragment.kt
+++ app/src/main/java/mx/itesm/bamx/fragments/HomeFragment.kt
@@ -15,6 +15,7 @@ import supportClasses.DonationAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+// comment
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
