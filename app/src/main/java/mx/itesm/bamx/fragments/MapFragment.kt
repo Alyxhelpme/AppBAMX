79c6d18e7dd5f784dc614f1727842a987a2f1656
Gael <gaelrodguez@hotmail.com>
2022-10-02 23:12:50 -0500

    Se muestra la info al dar tap en un marcador

--- app/src/main/java/mx/itesm/bamx/fragments/MapFragment.kt
+++ app/src/main/java/mx/itesm/bamx/fragments/MapFragment.kt
@@ -9,16 +9,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.QueryDocumentSnapshot
 import com.google.firebase.firestore.ktx.firestore
 import mx.itesm.bamx.MainActivity
 import mx.itesm.bamx.R
@@ -26,26 +30,32 @@ import mx.itesm.bamx.RegisterAssociateActivity
 import mx.itesm.bamx.SearchCenterActivity
 
 
-class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener {
+class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMarkerClickListener {
 
     private lateinit var map : GoogleMap
     private lateinit var collection : CollectionReference
     private lateinit var logOutButton : Button
     private lateinit var registerAssociateButton : Button
     private lateinit var firebaseAuth : FirebaseAuth
-    lateinit var centers : Array<LatLng>
+    private lateinit var centers : HashMap<String, QueryDocumentSnapshot>
 
+    private lateinit var organizationName : TextView
+    private lateinit var address : TextView
+    private lateinit var email : TextView
+    private lateinit var phoneNumber : TextView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
-        centers = emptyArray()
+
+        organizationName = view.findViewById(R.id.organizationName)
+        address = view.findViewById(R.id.address)
+        email = view.findViewById(R.id.email)
+        phoneNumber = view.findViewById(R.id.phoneNumber)
+
         registerAssociateButton = view.findViewById(R.id.becomeAssociateButton)
         registerAssociateButton.setOnClickListener{becomeAssociate()}
 
@@ -55,6 +65,8 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButton
             firebaseAuth.signOut()
             startActivity(Intent(this.context,MainActivity::class.java))
         }
+
+
 
         return view
     }
@@ -80,22 +92,30 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButton
         }
         val cameraPosition = LatLng(24.0220143,-101.3152273)
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(cameraPosition, 4f))
+        map.setOnMarkerClickListener(this)
         fetchCenters()
     }
 
     fun fetchCenters(){
         collection = Firebase.firestore.collection("centers")
         val requestCenters = collection.get()
+        centers = HashMap()
         requestCenters.addOnSuccessListener {
                 result ->
             for(document in result){
                 val lat = document.getDouble("lat")
                 val lng = document.getDouble("lng")
+                val approved = document.getBoolean("approved")
+                val id = document.id
+                val orgName = document.getString("organizationName")
                 var coordinates : LatLng
-                if(lat != null && lng != null){
+                if(lat != null && lng != null && approved == true){
                     coordinates = LatLng(lat, lng)
-                    centers.plus(coordinates)
-                    map.addMarker(MarkerOptions().position(coordinates).title("Center"))
+                    val marker = map.addMarker(MarkerOptions().position(coordinates).title(orgName))
+                    if (marker != null) {
+                        marker.tag = id
+                    }
+                    centers[id] = document
                 }
             }
         }. addOnFailureListener{
@@ -108,10 +128,26 @@ class MapFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMyLocationButton
         map.animateCamera(CameraUpdateFactory.zoomIn())
         return false
     }
+
+    override fun onMarkerClick(p0: Marker): Boolean{
+        val doc = centers[p0.tag]
+        if (doc != null) {
+            organizationName.text = doc.getString("organizationName")
+            address.text = doc.getString("address")
+            email.text = doc.getString("email")
+            phoneNumber.text = doc.getString("phoneNumber")
+        }
+        map.animateCamera(CameraUpdateFactory.zoomIn())
+        return false
+    }
 //endregion
 
     private fun becomeAssociate(){
         val intent = Intent(requireActivity(), RegisterAssociateActivity::class.java)
         startActivity(intent)
     }
+
+
+
+
 }
