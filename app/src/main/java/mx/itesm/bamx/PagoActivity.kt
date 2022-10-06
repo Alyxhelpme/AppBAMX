44cb97fe60374bc6df5287a8f211b2832d6602ce
Alyxhelpme <A01640585@itesm.mx>
2022-10-06 02:12:17 -0500

    PayPal checkout terminated

--- app/src/main/java/mx/itesm/bamx/PagoActivity.kt
+++ app/src/main/java/mx/itesm/bamx/PagoActivity.kt
@@ -2,20 +2,36 @@ package mx.itesm.bamx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.TextView
 
 class PagoActivity : AppCompatActivity() {
 
     private lateinit var textoEditable : EditText
+    lateinit var webview: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pago)
+        webview = findViewById(R.id.webView)
+        webview.settings.allowFileAccess = true
+        webview.settings.allowContentAccess = true
+        webview.settings.domStorageEnabled = true
+        webview.settings.javaScriptEnabled = true
 
+        webview.loadUrl("file:///android_asset/index.html")
+
+
+        webview.webViewClient = object : WebViewClient(){
+            override fun onLoadResource(view: WebView?, url: String?) {
+                super.onLoadResource(view, url)
+                webview.evaluateJavascript("replace('modifiable value','${carrito.toString()}')")    {}
+            }
+        }
         val textito = findViewById<TextView>(R.id.ejemploTV)
-        textito.text = "Hola"
         textito.text = carrito.toString()
     }
 }
