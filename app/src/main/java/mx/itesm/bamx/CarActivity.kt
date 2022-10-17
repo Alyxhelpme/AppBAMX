8a28a2a44e315bd6eea9dd3ff37c5d220a93f0c7
Juan Pablo <jppd0657@gmail.com>
2022-10-16 21:45:03 -0500

    sc and donation UI finished

--- app/src/main/java/mx/itesm/bamx/CarActivity.kt
+++ app/src/main/java/mx/itesm/bamx/CarActivity.kt
@@ -1,17 +1,24 @@
 package mx.itesm.bamx
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
 import supportClasses.CarAdapter
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class CarActivity : AppCompatActivity(), CarAdapter.CarListener, View.OnClickListener {
 
     lateinit var RecyclerView : RecyclerView
+    lateinit var payButton: Button
+    lateinit var backButton: Button
+    lateinit var totalTV : TextView
+    lateinit var emptyCarTV : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         println(nombresC)
@@ -20,6 +27,24 @@ class CarActivity : AppCompatActivity(), CarAdapter.CarListener, View.OnClickLis
         println("hola")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_car)
+
+        emptyCarTV = findViewById(R.id.emptyCar)
+        totalTV = findViewById(R.id.totalTVCar)
+        payButton = findViewById(R.id.payButton)
+        backButton = findViewById(R.id.backButton)
+
+        if (carrito == 0) {
+            emptyCarTV.text = "Carrito vacio. Agrega un elemento."
+            payButton.visibility = View.GONE
+            totalTV.text = ""
+        } else {
+            emptyCarTV.text = ""
+            totalTV.text = "Total: $" + carrito + " MXN"
+        }
+
+
+        backButton.setOnClickListener { onBackPressed() }
+        payButton.setOnClickListener { startActivity(Intent(this, PagoActivity::class.java)) }
 
         RecyclerView = findViewById(R.id.RV_ShoppingCar)
 
@@ -44,6 +69,12 @@ class CarActivity : AppCompatActivity(), CarAdapter.CarListener, View.OnClickLis
     }
 
     override fun onClick(p0: View?) {
-        TODO("Not yet implemented")
+        return
     }
+
+    private fun goPay() {
+        val intent = Intent(this, PaypalActivity::class.java)
+        startActivity(intent)
+    }
+
 }
